5756b79984fdb81190f33e69ecd1536494c41d68
Joe Hachem <joehachem290@gmail.com>
2024-07-27 09:14:43 +0300

    1 & 2 ✅

--- exercises/02_basic_calculator/06_while/src/lib.rs
+++ exercises/02_basic_calculator/06_while/src/lib.rs
@@ -1,10 +1,19 @@
+use std::os::unix::raw::nlink_t;
+
 // Rewrite the factorial function using a `while` loop.
 pub fn factorial(n: u32) -> u32 {
     // The `todo!()` macro is a placeholder that the compiler
     // interprets as "I'll get back to this later", thus
     // suppressing type errors.
     // It panics at runtime.
-    todo!()
+    let mut rep: u32 = 1;
+    let mut counter: u32 = 1;
+
+    while counter <= n {
+        rep *= counter;
+        counter += 1;
+    }
+    return rep;
 }
 
 #[cfg(test)]
